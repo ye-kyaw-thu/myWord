024f5aa1b477253a1cb91cfb1a8c337cfd8951f4
Ye Kyaw Thu <wasedakuma@gmail.com>
2021-09-11 11:25:03 +0700

    Update split-lt-24mb.sh

--- tools/split-lt-24mb.sh
+++ tools/split-lt-24mb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# for splitting bigram-phrase.bin,bigram-phrase.txt,bigram-word.bin and bigram-word.txt files into <=24MB files
+# Written by Ye Kyaw Thu, LST, NECTEC, Thailand
+
 for i in {bigram-phrase.bin,bigram-phrase.txt,bigram-word.bin,bigram-word.txt}
 do
     split -b 24M $i $i.small.
