cf00e35519e919c90b5c9b7e0c77d40fadc3804c
Ye Kyaw Thu <wasedakuma@gmail.com>
2021-09-11 11:26:04 +0700

    Update split-lt-24mb.sh

--- tools/split-lt-24mb.sh
+++ tools/split-lt-24mb.sh
@@ -2,6 +2,7 @@
 
 # for splitting bigram-phrase.bin,bigram-phrase.txt,bigram-word.bin and bigram-word.txt files into <=24MB files
 # Written by Ye Kyaw Thu, LST, NECTEC, Thailand
+# When you need to run, you have to move this script to under dictionary folder e.g. dict_ver1/
 
 for i in {bigram-phrase.bin,bigram-phrase.txt,bigram-word.bin,bigram-word.txt}
 do
